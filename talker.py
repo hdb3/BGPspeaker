f23219b21f445d9e62487a5ac683f08c4552774a
Nicholas Hart <github@hdb3.net>
2018-06-13 13:40:30 +0100

    Update talker.py

--- talker.py
+++ talker.py
@@ -1,17 +1,18 @@
-import os.path
 import errno
-import yaml
-import traceback
 import sys
 import socket
 import threading
 from time import sleep
 
-class Talker(Collector):
+class ActiveSession(Threading):
 
-    def __init__(self,*args,**kwargs):
-        super().__init__(*args,**kwargs)
-
+    def __init__(self,peer,timeout):
+        self.peer = peer
+        self.timeout = timeout
+    
+    def log_err(self,s):
+        print(s,file=sys.stderr)
+    
     def run(self):
         self.state = Connecting
         while True:
@@ -23,14 +24,14 @@ class Talker(Collector):
                         self.log_err("attempting connection to %s\n" % _name(self.address))
                     else:
                         self.log_err("reattempting connection to %s\n" % _name(self.address))
-                self.sock = socket.create_connection(self.address,1)
+                self.sock = socket.create_connection(self.peer,self.timeout)
                 self.remote_address = self.sock.getpeername()
                 self.sock.setblocking(True)
                 self.state = Connected
                 self.connections += 1
                 self.log_err("connected to %s\n" % _name(self.address))
                 self.connections += 1
-                session = Session(self.app,self.appconfig,self.name,self.remote_address,self.send,self.recv)
+                self.fsm()
                 self.sock.close()
                 self.sock.shutdown(socket.SHUT_RDWR)
             except (socket.error,socket.timeout) as e:
@@ -48,4 +49,9 @@ class Talker(Collector):
                 traceback.print_tb( sys.exc_info()[2],limit=9)
                 self.state = Error
                 break
-
+                
+        def fsm(self):
+            self.socket.send("Hello from %s" % str())
+            while True:
+                msg = self.socket.recv()
+                self.socket.send(msg)
