98bdd58efff3e3d9b94b4ac2764abf9198c06c4f
Nicholas Hart <nic@hdb3.net>
2018-06-18 18:20:35 +0100

    working with helloworl to detect local addresses

--- helloworld.py
+++ helloworld.py
@@ -13,12 +13,14 @@ def get_local_ipv4_addresses():
                 yield address_info['addr']
 
 peers = ["172.16.0.1", "172.16.0.2","172.16.0.3","172.16.0.4"]
-local_addresses = get_local_ipv4_addresses()
-_peers = list(map(IPv4Address,peers))
-__peers = []
-for _peer in _peers:
-    if _peer not in local_addresses:
-        print("using %s" % _peer)
-        __peers.append(_peer)
+local_addresses = list(get_local_ipv4_addresses())
+#print(local_addresses)
+#print(peers)
+_peers = []
+for peer in peers:
+    if peer not in local_addresses:
+        print("using %s" % peer)
+        _peers.append(peer)
 
-sm = SessionManager(__peers,10.0)
+__peers = list(map(IPv4Address,_peers))
+sm = SessionManager(__peers)
