f868898e9eae98a6a113f5ebffae9b15d3951720
Nicholas Hart <nic@hdb3.net>
2018-06-19 16:52:30 +0100

    works well enought t run the main loop over consecutive connecton requests.  Exit/keyboard interrup handling not doing what it should

--- sessionmanager.py
+++ sessionmanager.py
@@ -13,10 +13,8 @@
 # already one session for that peer.  A list of active peers is used for this purpose
 #
 
-import errno
-import sys
-import socket
-import concurrent.futures
+import sys, os, errno, socket, concurrent.futures
+from time import sleep
 
 _BGP_PORT = 179
 _NEW_ACTIVE_SOCKET = 1
@@ -49,63 +47,71 @@ class SessionManager:
             raise
         futures = []
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            #futures.append(executor.submit(self.get_passive_socket))
+            futures.append(executor.submit(self.get_passive_socket))
             self.peerlist = peerlist
             print("peerlist: ",str(peerlist))
             for peer in peerlist:
                 #peerstate = {}
                 #peers[peer] = peerstate
-                print("scheduling active session request for %s",str(peer))
+                print("scheduling active session request for",str(peer))
                 futures.append(executor.submit(self.get_active_socket,peer))
             print("starting main loop")
 
-            while True:
-                done,not_done = concurrent.futures.wait(futures, return_when=concurrent.futures.FIRST_COMPLETED)
-                futures = not_done
-                # completed futures may return new sockets, which require to run the FSM
-                # or old sockets from an exiting FSM
-                # or a timeout after unsuccesful active session request
-                # some of which may require to start another listener/talker
+            try:
+                while True:
+                    done,not_done = concurrent.futures.wait(futures, return_when=concurrent.futures.FIRST_COMPLETED)
+                    futures = list(not_done)
+                    # completed futures may return new sockets, which require to run the FSM
+                    # or old sockets from an exiting FSM
+                    # or a timeout after unsuccesful active session request
+                    # some of which may require to start another listener/talker
 
-                for f in done:
-                    status,sock,peer = f.result()
-                    if _NEW_ACTIVE_SOCKET == status or _NEW_PASSIVE_SOCKET == status:
-                        remote_address = sock.getpeername()
-                        local_address = sock.getsockname()
-                        # if the socket is a passive one the local port will be the well known one
-                        # if the socket is an active one the remote port will be the well known one
-                        # it should never be the case that either both or neither port is well known
-                        assert remote_address[1] != local_address[1]
-                        assert remote_address[1] == _BGP_PORT or local_address[1] == _BGP_PORT
-                        print("new connection to %s" % str(remote_address[0]))
-                        if _NEW_ACTIVE_SOCKET == status:
-                            assert remote_address[1] == _BGP_PORT
-                        else: # _NEW_PASSIVE_SOCKET == _BGP_PORT
-                            assert local_address[1] == _BGP_PORT
-                            print("rescheduling passive port listener")
-                            futures.append(executor.submit(self.get_passive_socket))
-                        if remote_address[0] not in self.active_peers:
-                            self.active_peers[remote_address[0]] = 0
-                        self.active_peers[remote_address[0]] += 1
-                        futures.append(executor.submit(self.fsm,sock,peer))
-                    elif _OLD_SOCKET == status:
-                        assert peer in self.active_peers
-                        self.active_peers[peer] -= 1
-                        assert self.active_peers[peer] >= 0
-                        # exiting FSM - check if we need to restart an active session request thread
-                        if self.active_peers[peer] == 0 and peer in self.peerlist:
-                            futures.append(executor.submit(self.get_active_socket,peer))
-                        try:
-                            sock.shutdown(socket.SHUT_RDWR)
-                            sock.close()
-                        except Exception as e:
-                            self.log_err("ignored exception closing socket: %s\n" % str(e))
+                    for f in done:
+                        # status,sock,peer = f.result()
+                        result = f.result()
+                        status,sock,peer = result
+                        print("Main loop event: %s" % str(result) )
+                        if _NEW_ACTIVE_SOCKET == status or _NEW_PASSIVE_SOCKET == status:
+                            remote_address = sock.getpeername()
+                            local_address = sock.getsockname()
+                            # if the socket is a passive one the local port will be the well known one
+                            # if the socket is an active one the remote port will be the well known one
+                            # it should never be the case that either both or neither port is well known
+                            assert remote_address[1] != local_address[1]
+                            assert remote_address[1] == _BGP_PORT or local_address[1] == _BGP_PORT
+                            print("new connection to %s" % str(remote_address[0]))
+                            if _NEW_ACTIVE_SOCKET == status:
+                                assert remote_address[1] == _BGP_PORT
+                            else: # _NEW_PASSIVE_SOCKET == _BGP_PORT
+                                assert local_address[1] == _BGP_PORT
+                                print("rescheduling passive port listener")
+                                futures.append(executor.submit(self.get_passive_socket))
+                            if remote_address[0] not in self.active_peers:
+                                self.active_peers[remote_address[0]] = 0
+                            self.active_peers[remote_address[0]] += 1
+                            futures.append(executor.submit(self.fsm,sock,peer))
+                        elif _OLD_SOCKET == status:
+                            assert peer in self.active_peers
+                            self.active_peers[peer] -= 1
+                            assert self.active_peers[peer] >= 0
+                            # exiting FSM - check if we need to restart an active session request thread
+                            if self.active_peers[peer] == 0 and peer in self.peerlist:
+                                futures.append(executor.submit(self.get_active_socket,peer))
+                            try:
+                                sock.shutdown(socket.SHUT_RDWR)
+                                sock.close()
+                            except Exception as e:
+                                print("ignored exception closing socket: %s\n" % str(e))
 
-                    elif _TIMEOUT_SOCKET == status:
-                        if peer in self.active_peers and self.active_peers[peer] == 0 and peer in self.peerlist:
-                            futures.append(executor.submit(self.get_active_socket,peer))
-                    else:
-                        assert False
+                        elif _TIMEOUT_SOCKET == status:
+                            assert peer in self.peerlist
+                            if peer not in self.active_peers or self.active_peers[peer] == 0:
+                                futures.append(executor.submit(self.get_active_socket,peer))
+                        else:
+                            assert False
+            except KeyboardInterrupt:
+                executor.shutdown(wait=False)
+                kill_child_processes(os.getpid())
 
     def get_passive_socket(self):
 
@@ -137,6 +143,10 @@ class SessionManager:
         except socket.timeout as e:
             sock.close()
             return (_TIMEOUT_SOCKET,None,peer)
+        except ConnectionError as e:
+            print("peer %s rejected connection %s" % (str(peer),e))
+            sleep(self.timeout) # strictly this should be the 'idle hold timer' value which could be differetn to the connection timer.....
+            return (_TIMEOUT_SOCKET,None,peer)
         except socket.error as e:
             print("unknown socket error %s" % e)
             raise
@@ -154,6 +164,8 @@ class SessionManager:
             print("connection complete to %s" % str(remote_address))
             sock.setblocking(True)
             return (_NEW_ACTIVE_SOCKET,sock,remote_address[0])
+        finally:
+            print("exiting from get_active_socket")
 
     def fsm(self,sock,peer):
         sock.send("Hello from %s" % str())
@@ -167,3 +179,17 @@ class SessionManager:
     def _fsm(self,sock,peer):
         self.fsm(self,sock,peer)
         return (_OLD_SOCKET,sock,peer)
+
+# ugly hack because concurrent.futures has no way to kill child threads
+# see https://stackoverflow.com/questions/42782953/python-concurrent-futures-how-to-make-it-cancelable/
+
+import signal, psutil
+
+def kill_child_processes(parent_pid, sig=signal.SIGTERM):
+    try:
+        parent = psutil.Process(parent_pid)
+    except psutil.NoSuchProcess:
+        return
+    children = parent.children(recursive=True)
+    for process in children:
+        process.send_signal(sig)
